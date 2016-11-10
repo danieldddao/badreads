4e8e7643186b57a273b73d974c04c2b70a20905f
Kunal Bharathi <kunal-bharathi@uiowa.edu>
2016-11-10 17:24:16 +0000

    added reference for interest in user db migration

--- db/migrate/20161108195739_create_interests.rb
+++ db/migrate/20161108195739_create_interests.rb
@@ -1,7 +1,8 @@
 class CreateInterests < ActiveRecord::Migration[5.0]
   def change
     create_table :interests do |t|
-
+      t.string :genre
+      t.references 'user'
       t.timestamps
     end
   end
