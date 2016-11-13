d0b7ec6610488e74c863176d1a7db68547bbbd49
Kunal Bharathi <kunal-bharathi@uiowa.edu>
2016-11-13 01:34:37 +0000

    Fixed strucutral/logical Error for some associations

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,12 @@ class GroupsController < ApplicationController
     def view_calendar
         #render simple calendar
     end
+    
+    def create_event
+        #create a book club event
+    end
+    
+    def sign_up_for_event
+        #add users to a current event
+    end
 end
