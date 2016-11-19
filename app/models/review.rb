2ef873992338f272309113ea103848c8301cec5a
Daniel Dao <daniel.dddao@gmail.com>
2016-11-19 23:09:15 +0000

    review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,17 @@
 class Review < ActiveRecord::Base
     belongs_to :book
     belongs_to :user
+    # after_save :update_review_count
     validates :star, presence: true
     validates :comment, presence: true
 
+    # private
+    # def update_review_count
+    #     puts "Update review"
+    #     puts self.book_id
+    #     book = Book.find(self.book_id)
+    #     puts book.reviews.count
+    #     book.reviews_count = book.reviews.count
+    #     book.save
+    # end
 end
