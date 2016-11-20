02d810cda8a09e9aa1928cd43cd62a906f6dc40a
Daniel Dao <daniel.dddao@gmail.com>
2016-11-20 20:58:16 +0000

    fixed review counts

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,8 +9,7 @@ class ReviewsController < ApplicationController
       if !@current_user_has_review
         @review = @book.reviews.new(:comment => params[:new_review_comment], :star => params[:rating])
         @current_user.reviews << @review
-        @book.reviews_count = @book.reviews.count
-        if @review.save && @book.save
+        if @review.save
           flash[:notice] = "Your Review for '#{@book.title}' successfully submitted!"
           redirect_to book_path(@book)
         else
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,12 @@ class Review < ActiveRecord::Base
     belongs_to :user
     validates :star, presence: true
     validates :comment, presence: true
+    after_save :update_review_count
 
+    private
+        def update_review_count
+            book = Book.find(self.book_id)
+            book.reviews_count = book.reviews.count
+            book.save
+        end
 end
