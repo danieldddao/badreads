a0eb719545de07a7f8d538530d7e3a3dfa4dbf82
Daniel Dao <daniel.dddao@gmail.com>
2016-11-22 15:19:22 +0000

    finished following users

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,12 +4,16 @@ class FollowsController < ApplicationController
   def create
     if @current_user
       user = User.find(params[:user_id])
-      follow = user.follows.new(:follower_id => @current_user.id);
-      begin
-        follow.save
-        flash[:warning] = "Successfully followed user: #{user.first_name} #{user.last_name}"
-      rescue ActiveRecord::RecordNotUnique
-      flash[:warning] = "Error! You already followed this user!"
+      if @current_user != user.id
+        follow = user.follows.new(:follower_id => @current_user.id);
+        begin
+          follow.save
+          flash[:warning] = "Successfully followed user: #{user.first_name} #{user.last_name}"
+        rescue ActiveRecord::RecordNotUnique
+        flash[:warning] = "Error! You already followed this user!"
+        end
+      else
+        flash[:warning] = "Error! You can't follow yourself!"
       end
     else
       flash[:warning] = "Error! Please log-in to follow a user!"
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,4 +3,14 @@ class Follow < ActiveRecord::Base
     validates :follower_id, presence: true
     validates :user_id, presence: true
 
+    def self.has_current_user_followed_this_user(current_user_id, this_user_id)
+        follows = Follow.where(:follower_id => current_user_id)
+        if !follows.empty?
+            follows.each { |follow| 
+                return true if follow.user_id == this_user_id
+            }
+        end
+        return false
+    end
+
 end
